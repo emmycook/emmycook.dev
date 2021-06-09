5ec6bf51b15965182f524e72673d14f3e874cead
emmycook <82702274+emmycook@users.noreply.github.com>
2021-06-10 10:52:32 +1200

    Create index.tsx

--- src/contents/index.tsx
+++ src/contents/index.tsx
@@ -8,16 +8,6 @@ import { Template } from '../template';
 
 export const SiteContents = (
   <main>
-    {Intro}
-    {Example}
-    {Tokens}
-    <Template id="main-footer">
-      <footer>
-        <h4>Licence</h4>
-        <p>
-          This website and <a href="https://github.com/virtualstate/virtualstate.dev" target="_blank" rel="noopener noreferrer">associated GitHub respository</a> is licensed under the <a href="https://creativecommons.org/publicdomain/zero/1.0/" target="_blank" rel="noopener noreferrer">CC0 1.0 Universal</a> license.
-        </p>
-      </footer>
-    </Template>
+    Hello World
   </main>
 );
